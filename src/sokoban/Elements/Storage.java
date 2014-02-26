dd61d264ee0cb80a91d34a8f15876d2806247373
gael033 <160794@supinfo.com>
2014-02-26 16:09:15 +0100

    MAJ de Storage

--- src/sokoban/Elements/Storage.java
+++ src/sokoban/Elements/Storage.java
@@ -1,9 +1,30 @@
 package sokoban.Elements;
 
-public class Storage {
+public class Storage extends ElementNonMovable{
 
+	//=================================
+	//===========Attributs :===========
+	//=================================
+	
+	String id="O";
+	
+	
+	//=================================
+	//===========Méthodes :============
+	//=================================
+	
+	/*
+	 * Constructeur 
+	 */
 	public Storage() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/*
+	 * Getter de l'ID : "O"
+	 */
+	public String Show()
+	{
+		return id;
+	}
 }
