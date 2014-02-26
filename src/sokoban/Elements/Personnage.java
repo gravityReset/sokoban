a8aa95ce814ee654e2477a886128c9c9d53ef1a7
gael033 <160794@supinfo.com>
2014-02-26 16:08:59 +0100

    MAJ de Personnage

--- src/sokoban/Elements/Personnage.java
+++ src/sokoban/Elements/Personnage.java
@@ -1,9 +1,42 @@
 package sokoban.Elements;
 
-public class Personnage {
+public class Personnage extends ElementMovable{
 
+	//=================================
+	//===========Attributs :===========
+	//=================================
+	
+	String id="X";
+	
+	
+	//=================================
+	//===========Méthodes :============
+	//=================================
+	
+	
+	/*
+	 * Constructeur de Personnage :
+	 */
 	public Personnage() {
 		// TODO Auto-generated constructor stub
+		
 	}
+	
+	/*
+	 * Getter de id : "X"
+	 */
+	@Override
+	public String Show()
+	{
+		return id;
+	}
+
+	@Override
+	public void Deplacer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
