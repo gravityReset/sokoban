948cb190b7ad0a50f457a80effacf7a628fd4a82
gael033 <160794@supinfo.com>
2014-02-26 16:12:58 +0100

    Element Vide => BlankSpace

---
+++ src/sokoban/Elements/ElementVide.java
@@ -0,0 +1,30 @@
+package sokoban.Elements;
+
+public class ElementVide extends ElementNonMovable{
+
+	//=================================
+	//===========Attributs :===========
+	//=================================
+	
+	String id=" ";
+	
+	
+	//=================================
+	//===========Méthodes :============
+	//=================================
+	
+	/*
+	 * Constructeur :
+	 */
+	public ElementVide() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public String Show() {
+		// TODO Auto-generated method stub
+		return id;
+	}
+
+}
