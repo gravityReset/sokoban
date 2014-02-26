2aecb01b1cbfe0e0bb43e803aa855cec845d82dc
kraenys <158243@supinfo.com>
2014-02-26 17:51:51 +0100

    Gestion du score, et écriture de celui-ci dans un .txt

--- src/sokoban/Utils/Score.java
+++ src/sokoban/Utils/Score.java
@@ -1,5 +1,36 @@
 package sokoban.Utils;
 
-public class Score {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Score{
+	
+
+	public static void AddScore(long score, int level){
+		
+		//Recuperation du pseudo et du chrono
+		System.out.println("indiquez votre pseudo");
+		EntreeClavier e = new EntreeClavier();
+		String pseudo = e.NextLine();
+		System.out.println("Votre chrono est de:"+ score);		
+		
+	}
+	public static void EnregistrementScore(long score, String pseudo, int level)
+	{
+	//Ecriture de ces derniers dans un fichier texte
+			  try {
+				FileWriter fw = new FileWriter("Score.txt", true);
+				
+				fw.write(pseudo);
+				fw.write((int) score);
+				fw.write(level);
+				fw.write("\n");
+				fw.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+  
+	}
 }
