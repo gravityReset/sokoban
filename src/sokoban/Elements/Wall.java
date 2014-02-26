836cf23ef86db9ec5d91372f892d16cf6f88eac1
gael033 <160794@supinfo.com>
2014-02-26 16:09:37 +0100

    MAJ de wall

--- src/sokoban/Elements/Wall.java
+++ src/sokoban/Elements/Wall.java
@@ -1,9 +1,31 @@
 package sokoban.Elements;
 
-public class Wall {
+public class Wall extends ElementNonMovable {
 
+	//=================================
+	//===========Attributs :===========
+	//=================================
+	
+	String id="=";
+	
+	
+	//=================================
+	//===========Méthodes :============
+	//=================================
+	
+	/*
+	 * Constructeur de Wall
+	 */
 	public Wall() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 * Getter de id : "="
+	 */
+	public String Show()
+	{
+		return id;
 	}
 
 }
