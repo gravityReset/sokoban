861b461031063229dca5f251ffbb5559db0beeb7
GravityReset <152565@supinfo.com>
2014-02-26 12:27:27 +0100

    map modifier un peu

--- src/sokoban/Map.java
+++ src/sokoban/Map.java
@@ -3,10 +3,20 @@
  */
 package sokoban;
 
+import java.util.List;
+
 /**
  * @author 15824_000
  *
  */
 public class Map {
-
+	private int level;
+	private List<List<Element>> structure ;
+	
+	
+	public Map(int level )
+	{
+		
+	}
+	
 }
