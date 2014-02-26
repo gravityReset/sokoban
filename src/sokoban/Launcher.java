e8536478b90de1971617f05e9f1e16c3a79cd057
NeoX00 <meta.neox@gmail.com>
2014-02-26 13:33:06 +0100

    Class Timer + test dans launcher

--- src/sokoban/Launcher.java
+++ src/sokoban/Launcher.java
@@ -7,6 +7,12 @@ public class Launcher
 	public static void main(String[] args)
 	{
 		System.out.println("coucou");
+		// test timer
+		Timer.Go_Chrono();
+		for(double i=0;i<1000000000;i++){
+			
+		}
+		Timer.Stop_Chrono();
 	}
 
 }
---
+++ src/sokoban/Timer.java
@@ -0,0 +1,18 @@
+package sokoban;
+
+public class Timer {
+	// Fonctions pour le chronometre 
+	static long chrono = 0 ; 
+	 
+	// Lancement du chrono 
+	static void Go_Chrono() { 
+	chrono = java.lang.System.currentTimeMillis() ; 
+	} 
+	 
+	// Arret du chrono 
+	static void Stop_Chrono() { 
+	long chrono2 = java.lang.System.currentTimeMillis() ; 
+	long temps = chrono2 - chrono ; 
+	System.out.println("Temps ecoule = " + temps + " ms") ; 
+	} 
+}
