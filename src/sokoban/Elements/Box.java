50d1410462746b7ff880dab38cc146fcef516440
gael033 <160794@supinfo.com>
2014-02-26 15:57:20 +0100

    MAJ de Classe Box

--- src/sokoban/Elements/Box.java
+++ src/sokoban/Elements/Box.java
@@ -1,25 +1,49 @@
 package sokoban.Elements;
 
-public class Box {
+public class Box extends ElementMovable {
 
+	
+	//=================================
+	//===========Attributs :===========
+	//=================================
+	
+	String id="B";
+	
+	
+	//=================================
+	//===========Méthodes :============
+	//=================================
+	
+	/**
+	 * @author 160794
+	 * Constructeur
+	 *
+	 */
 	public Box() {
 		// TODO Auto-generated constructor stub
 		
-		//=================================
-		//===========Attributs :===========
-		//=================================
 		
-		String id="B";
+	}
+	
+	/*
+	 * (getter de id)
+	 * @160794
+	 */
+	public String Show()
+	{
+		return id;
 		
+	}
+			
+	
+
+	/*
+	 * (Déplace un objet mobil)
+	 * @160794
+	 */
+	public void Deplacer() {
+		// TODO Auto-generated method stub
 		
-		//=================================
-		//===========Méthodes :============
-		//=================================
-		
-		public String show()
-		{
-			return id;
-		}
 	}
 
 }
