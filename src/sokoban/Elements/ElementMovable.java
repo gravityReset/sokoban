eb209f8252aa5fb2a001fc63cd09c89362d3de6e
gael033 <160794@supinfo.com>
2014-02-26 17:24:59 +0100

    MAJ (problemen méthode déplacer) de la classe ElementMovable

--- src/sokoban/Elements/ElementMovable.java
+++ src/sokoban/Elements/ElementMovable.java
@@ -1,5 +1,10 @@
 package sokoban.Elements;
 
+import java.util.ArrayList;
+
+import sokoban.Map;
+import sun.nio.cs.ext.ISCII91;
+
 
 public abstract class ElementMovable  extends Element {
 	
@@ -14,9 +19,34 @@ public abstract class ElementMovable  extends Element {
 	//=================================
 	
 	public abstract String Show();
-	public void Deplacer(String s)
+	public void Deplacer(String s, Map m)
 	{
+		//On récupère la map
+		ArrayList< ArrayList<Element>> map = m.getStructure();
+		switch (s) {
 		
+		case "z": // Si on veux déplacer vers le haut :
+		{
+			//Si on a un ElementNonMovable :
+			if( map[this.CoordonneeX][this.CoordonneeY--] instanceof ElementNonMovable)
+			{
+				break;
+			}
+			//Si on a un ElementMovable :
+			else if ( map[this.CoordonneeX][this.CoordonneeY] instanceof ElementMovable) {
+				Deplacer("z", m);
+				break;
+			}
+			else
+			{
+				break;
+			}
+			
+		}
+			
+		default:
+			break;
+		}
 	}
 	
 }
