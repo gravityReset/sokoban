4c70d11dba6cbc03557fee5384206588760720d5
gael033 <160794@supinfo.com>
2014-02-26 15:39:09 +0100

    Box

---
+++ src/sokoban/Elements/Box.java
@@ -0,0 +1,25 @@
+package sokoban.Elements;
+
+public class Box {
+
+	public Box() {
+		// TODO Auto-generated constructor stub
+		
+		//=================================
+		//===========Attributs :===========
+		//=================================
+		
+		String id="B";
+		
+		
+		//=================================
+		//===========Méthodes :============
+		//=================================
+		
+		public String show()
+		{
+			return id;
+		}
+	}
+
+}
