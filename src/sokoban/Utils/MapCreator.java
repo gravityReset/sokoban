49422b0ae358771e53a4417ed3dc2cb236464dbc
GravityReset <152565@supinfo.com>
2014-02-28 11:25:13 +0100

    MapCreator

---
+++ src/sokoban/Utils/MapCreator.java
@@ -0,0 +1,19 @@
+package sokoban.Utils;
+import java.util.ArrayList;
+
+
+public class MapCreator
+{
+	ArrayList<String> map; 
+	
+	MapCreator()
+	{
+		map = new ArrayList<String>();
+		
+		do
+		{
+			
+		}while(map.get(map.size()) != "exit");
+	}
+	
+}
