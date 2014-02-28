1860a2723108c1a81c33b3d099725e2b2558d607
GravityReset <152565@supinfo.com>
2014-02-28 14:37:05 +0100

    Map creator constructeur terminé

--- src/sokoban/Utils/MapCreator.java
+++ src/sokoban/Utils/MapCreator.java
@@ -1,19 +1,59 @@
-package sokoban.Utils;
-import java.util.ArrayList;
 
+package sokoban.Utils;
+
+import java.util.ArrayList;
 
 public class MapCreator
 {
-	ArrayList<String> map; 
-	
+
+	ArrayList<String> map;
+
+
 	MapCreator()
 	{
+		Boolean personnage = false;
+
 		map = new ArrayList<String>();
-		
+		EntreeClavier ec = new EntreeClavier();
 		do
 		{
-			
-		}while(map.get(map.size()) != "exit");
+			String m = ec.NextLine();
+			String stringToSave = ""; // string qu'on va enregistrer après
+										// chaque truc
+
+			if (m != "exit")
+			{
+				for (int i = 0; i < m.length(); i++)// on parcours la ligne pour
+													// vérifier les entrés
+				{
+					if (m.charAt(i) == 'X') // si c'est un personnage
+						if (!personnage) // si il y en a
+						{
+							personnage = true; // on passe a vraie
+							stringToSave += m.charAt(i);
+						}
+						else
+							stringToSave += ' ';
+
+					// les entrés non valide sont remplacé par espace
+					else if (m.charAt(i) != ' ' && m.charAt(i) != 'O'
+							&& m.charAt(i) != 'B' && m.charAt(i) != '=')
+						stringToSave += ' ';
+
+					else
+						stringToSave += m.charAt(i);
+
+				}
+
+				map.add(stringToSave);// on ajoute la ligne
+			}
+
+		} while (map.get(map.size()) != "exit");
 	}
 	
+	void Save()
+	{
+		
+	}
+
 }
