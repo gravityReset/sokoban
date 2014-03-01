f583d143378abe2d6710759920298315f25576aa
gael033 <160794@supinfo.com>
2014-03-01 15:21:23 +0100

    maj

--- src/sokoban/Launcher.java
+++ src/sokoban/Launcher.java
@@ -15,91 +15,100 @@ public class Launcher
 
 	public static void main(String[] args) 
 	{
-		System.out.println("coucou");
-		 
-		String choixJeu = args[1];
-		
-		switch(choixJeu)
+		try
 		{
-			case "--create":
+			System.out.println("coucou");
+			 
+			String choixJeu = args[0];
+			
+			switch(choixJeu)
 			{
-				MapCreator mc = new MapCreator();
-				mc.Save();
-				break;
-			}
-			case "--level":
-			{
-				String lvl=args[2];
-				
-				// ====================== JEU ========================
-				int level=Integer.parseInt(lvl);
-				Map map = new Map(level);
-				map.Load();
-				map.Show();
-				
-				Timer.StartTimer();
-				
-				int i=0;
-				int nbStorage=0;
-				int nbStorageTrue;
-				
-				
-				// On compte le nombre d'éléments Storage non-remplis au début de la map :
-				for (ArrayList<ElementNonMovable> nomStorage : map.getStructure() ) {
-					for (ElementNonMovable Storage : nomStorage) {
-						if(Storage instanceof Storage)
-						{
-							nbStorage +=1;
-						}
-					}
-				} 
-
-				nbStorageTrue = nbStorage;
-				
-				System.out.println(nbStorage);
-				
-				while(i!=100)
-
+				case "--create":
 				{
-					//On compte le nombre de Storage full:
+					MapCreator mc = new MapCreator();
+					mc.Save();
+					break;
+				}
+				case "--level":
+				{
+					System.out.println("tu as tape --level");
+					String lvl=args[1];
+					
+					// ====================== JEU ========================
+					int level=Integer.parseInt(lvl);
+					Map map = new Map(level);
+					map.Load();
+					map.Show();
+					
+					Timer.StartTimer();
+					
+					int i=0;
+					int nbStorage=0;
+					int nbStorageTrue;
+					
+					
+					// On compte le nombre d'éléments Storage non-remplis au début de la map :
 					for (ArrayList<ElementNonMovable> nomStorage : map.getStructure() ) {
 						for (ElementNonMovable Storage : nomStorage) {
 							if(Storage instanceof Storage)
 							{
-								if(Storage.full==true)
-									nbStorageTrue -=1;
-								
+								nbStorage +=1;
 							}
 						}
-					}
-					
-					// Si tout les Storage sont rempli, on arrete la partie :
-					if (nbStorageTrue ==0)
-						break;
-					else
-					{
-						nbStorageTrue = nbStorage;
-						map.Deplacer();
-						map.Show();
-						i++;
-					}
-					
-					
+					} 
 
+					nbStorageTrue = nbStorage;
+					
+					System.out.println(nbStorage);
+					
+					while(i!=100)
+
+					{
+						//On compte le nombre de Storage full:
+						for (ArrayList<ElementNonMovable> nomStorage : map.getStructure() ) {
+							for (ElementNonMovable Storage : nomStorage) {
+								if(Storage instanceof Storage)
+								{
+									if(Storage.full==true)
+										nbStorageTrue -=1;
+									
+								}
+							}
+						}
+						
+						// Si tout les Storage sont rempli, on arrete la partie :
+						if (nbStorageTrue ==0)
+							break;
+						else
+						{
+							nbStorageTrue = nbStorage;
+							map.Deplacer();
+							map.Show();
+							i++;
+						}
+						
+						
+
+					}
+					
+					// ---------- test timer (attendre 3sec) -------------
+					long score = Timer.StopTimer();
+					System.out.println(score);
+					//----------------- Fin Test Timer ------------------- 
+					Score.AddScore(score, level);
+					
+					// =================== FIN JEU =======================
+					break;
 				}
-				
-				// ---------- test timer (attendre 3sec) -------------
-				long score = Timer.StopTimer();
-				System.out.println(score);
-				//----------------- Fin Test Timer ------------------- 
-				Score.AddScore(score, level);
-				
-				// =================== FIN JEU =======================
-				break;
+				default:
+					break;
 			}
-			default:
-				break;
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
 		}
+	}
+	
 		
 		
 		
